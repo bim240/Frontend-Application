557440cc5bb8f095b148c89503e7c7cf287ce3f3
bim240 <bimlendu357@gmail.com>
2021-03-10 21:05:52 +0530

    task complete

---
+++ src/containers/App/index.js
@@ -0,0 +1,60 @@
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { Switch, Route, withRouter } from "react-router-dom";
+import { injectReducer, injectSaga } from "redux-injectors";
+import reducer from "./reducer";
+import saga from "./saga";
+import Header from "components/Header";
+import Home from "containers/Home";
+import Product from "containers/Product";
+import { AppWrapper } from "./StyledComponents";
+import NavBar from "components/Navbar";
+import RightSection from "containers/RightSection";
+
+const App = (props) => {
+  const { user, company } = props;
+  return (
+    <AppWrapper>
+      <Header />
+      <section className="app_all_section">
+        <div className="left_section">
+          <NavBar />
+        </div>
+        <div className="middle_section">
+          <Switch>
+            <Route exact path={"/"} component={Home} />
+            <Route exact path={"/product"} component={Product} />
+            <Route exact path={"*"} component={Home} />
+          </Switch>
+        </div>
+        {user && company && (
+          <div className="right_section">
+            <RightSection user={user} company={company} />
+          </div>
+        )}
+      </section>
+    </AppWrapper>
+  );
+};
+
+function mapStateToProps(state) {
+  // all the props from redux
+  return {
+    user: state.app.user,
+    company: state.app.company,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  // connect all the function for disipacting action
+  return {};
+}
+
+const withConnect = connect(mapStateToProps, mapDispatchToProps);
+
+export default compose(
+  injectReducer({ key: "app", reducer }),
+  injectSaga({ key: "app", saga }),
+  withConnect,
+  withRouter
+)(App);
---
+++ src/containers/Product/saga.js
@@ -0,0 +1,22 @@
+import { takeLatest, call, put } from "redux-saga/effects";
+import request from "utils/request";
+import { setAllInfo } from "./action";
+import { GET_ALL_INFO } from "./constants";
+
+export function* requestAllInfo() {
+  try {
+    const requestURL = `/product/6781/`;
+    const response = yield call(request, {
+      method: "GET",
+      url: requestURL,
+    });
+
+    yield put(setAllInfo(response));
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export default function* defaultSaga() {
+  yield takeLatest(GET_ALL_INFO, requestAllInfo);
+}
